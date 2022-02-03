27ab92d4f94db47713947cce9974faa5f6b7b68e
wishkim99 <zzsowon@naver.com>
2022-02-03 09:01:53 +0900

    guestbook02 수정

--- guestbook02/src/main/java/com/poscoict/guestbook/controller/guestbookController.java
+++ guestbook02/src/main/java/com/poscoict/guestbook/controller/guestbookController.java
@@ -15,58 +15,47 @@ import com.poscoict.guestbook.vo.guestbookVo;
 public class guestbookController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
-
-		String actionName = request.getParameter("gb");
-
-		if ("form".equals(actionName)) {
-			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/index.jsp");
+		
+		String action = request.getParameter("a");
+		
+		if("deleteform".equals(action)) {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/deleteform.jsp");
 			rd.forward(request, response);
-		} else if ("add".equals(actionName)) {
+		} else if("delete".equals(action)) {
+			String no = request.getParameter("no");
+			String password = request.getParameter("password");
+			
+			guestbookVo vo = new guestbookVo();
+			vo.setNo(Integer.parseInt(no));
+			vo.setPassword(password);
+			
+			new guestbookDao().delete(vo);
+			response.sendRedirect(request.getContextPath() + "/gb");
+		} else if("add".equals(action)) {
 			String name = request.getParameter("name");
 			String password = request.getParameter("password");
 			String message = request.getParameter("message");
-
+			
 			guestbookVo vo = new guestbookVo();
 			vo.setName(name);
 			vo.setPassword(password);
 			vo.setMessage(message);
-
+			
 			new guestbookDao().insert(vo);
-
 			response.sendRedirect(request.getContextPath() + "/gb");
+		} else {
+			List<guestbookVo> list = new guestbookDao().findAll();
 			
-		}else if("delete".equals(actionName)) {
-			int no = Integer.valueOf(request.getParameter("no"));
-			String password= (String)request.getParameter("password");
-
-			
-			guestbookVo vo = new guestbookVo();
-			vo.setNo(no);
-			vo.setPassword(password);                             
-			
-			new guestbookDao().delete(vo);
-			
-			response.sendRedirect(request.getContextPath() + "/gb");
-		}
-		else {
-			guestbookDao dao = new guestbookDao();
-			List<guestbookVo> list = dao.findAll();
-
 			request.setAttribute("list", list);
-
+			
 			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/index.jsp");
 			rd.forward(request, response);
 		}
-
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
-
 }
--- guestbook02/src/main/webapp/WEB-INF/views/index.jsp
+++ guestbook02/src/main/webapp/WEB-INF/views/index.jsp
@@ -1,62 +1,52 @@
-<%@page import="com.poscoict.guestbook.dao.guestbookDao"%>
 <%@page import="java.util.List"%>
-<%@page import="java.util.ArrayList"%>
+<%@page import="com.poscoict.guestbook.dao.guestbookDao"%>
 <%@page import="com.poscoict.guestbook.vo.guestbookVo"%>
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%
+	List<guestbookVo> list = (List<guestbookVo>)request.getAttribute("list");
+%>
+<!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>방명록</title>
 </head>
 <body>
-
-	<form action="/guestbook02/add.jsp" method="post">
+	<form action="<%=request.getContextPath() %>/gb" method="post">
+		<input type="hidden" name="a" value="add">
 		<table border=1 width=500>
 			<tr>
-				<td>이름</td>
-				<td><input type="text" name="name"></td>
-				<td>비밀번호</td>
-				<td><input type="password" name="password"></td>
+				<td>이름</td><td><input type="text" name="name"></td>
+				<td>비밀번호</td><td><input type="password" name="password"></td>
 			</tr>
 			<tr>
 				<td colspan=4><textarea name="message" cols=60 rows=5></textarea></td>
 			</tr>
 			<tr>
-				<td colspan=4 align=right><input type="submit" VALUE=" 등록 "></td>
+				<td colspan=4 align=right><input type="submit" VALUE="등록"></td>
 			</tr>
 		</table>
 	</form>
-
+	
 	<%
-	guestbookDao dao = new guestbookDao();
-			List<guestbookVo> list = dao.findAll();
-			
-			List<guestbookVo> list1 = new guestbookDao().findAll();
+		int count = list.size();
+		int index = 0;
+		for(guestbookVo vo : list){
 	%>
+		<br>
+		<table width=510 border=1>
+			<tr>
+				<td>[<%=count-index++ %>]</td>
+				<td><%=vo.getName() %></td>
+				<td><%=vo.getReg_date() %></td>
+				<td><a href="<%=request.getContextPath() %>/gb?a=deleteform&no=<%=vo.getNo() %>">삭제</a></td>
+			</tr>
+			<tr>
+				<td colspan=4><%=vo.getMessage().replaceAll("\n", "<br/>") %></td>
+			</tr>
+		</table>
 	<%
-	int count = list1.size();
-			int index = 0;
-			for(guestbookVo vo : list1){
-	%>
-	<br>
-	<table width=510 border=1>
-
-
-		<tr>
-			<td>[<%=count-index++ %>]
-			</td>
-			<td><%=vo.getName()%></td>
-			<td><%=vo.getReg_date()%></td>
-
-			<td><a href="/guestbook02/deleteform.jsp?no=<%=vo.getNo()%>">삭제</a></td>
-		</tr>
-		<tr>
-			<td colspan=4><%=vo.getMessage()%></td>
-		</tr>
-		<%
 		}
-		%>
-	</table>
+	%>
 </body>
 </html>
